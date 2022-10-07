4a8fb1ac13b206b9a2c2d0bca65ca8d511b78677
BATOER-0605 <dougahsk@outlook.com>
2022-10-07 14:07:44 +0900

    未完成

--- 10_07/main.c
+++ 10_07/main.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 
-void kass(FILE *in,FILE *out);
+void kuso(FILE *in,FILE *out);
 
 int main(){
     FILE *INPUT1 = fopen("input1.txt","r");
     FILE *OUTPUT = fopen("output.csv","w");
 
-    kass(INPUT1,OUTPUT);
+    kuso(INPUT1,OUTPUT);
 }
 
-void kass(FILE *in,FILE *out){
+void kuso(FILE *in,FILE *out) {
     float IN[3][3];
     float x[3],y[3],z[3];
-    int t=0;
+    int t=0,tt;
     int n=0;
-    printf("分割数を入れんかいごらぁぁぁぁぁぁぁぁぁ!!!!!!!!!\n");
-    scanf("%d",&n);
+    float *X,*Y,*Z;
+    int i;
 
-    t = n /10;
-    
-    for(int i=0;i<3;i++){
+    for(i=0;i<3;i++){
         fscanf(in,"%f %f %f\n",&IN[i][0],&IN[i][1],&IN[i][2]);
     }
 
-    for(int i=0;i<3;i++){
-        x[i] = (IN[i+1][0] - IN[i][0]) * t + IN[i][0];
-        y[i] = (IN[i+1][1] - IN[i][1]) * t + IN[1][1];
-        z[i] = (IN[i+1][2] - IN[i][2]) * t + IN[i][2];
-    }
+    printf("分割数を入れんかいごらぁぁぁぁぁぁぁぁぁ!!!!!!!!!\n");
+    scanf("%d",&n);
+    tt = n/10;
 
-    for(int i=0;i<3;i++){
-        fprintf(out,"%f,%f,%f\n",x[i],y[i],z[i]);
+    i=0;
+
+    for(i=0;i<=n-1;i++){
+
+        *X = (IN[i+1][0] - IN[i][0]) * t + IN[i][0];
+        *Y = (IN[i+1][1] - IN[i][1]) * t + IN[i][1];
+        *Z = (IN[i+1][2] - IN[i][2]) * t + IN[i][2];
+        printf("%f,%f,%f\n", *X, *Y,*Z);
+        fprintf(out, "%f,%f,%f\n", *X, *Y,*Z);
     }
 
     fclose(in);
     fclose(out);
-
 }
