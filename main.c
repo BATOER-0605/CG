2de760834b95177dc6358403b3bc25b84002c0d4
BATOER-0605 <nem0605@outlook.com>
2022-10-31 12:20:30 +0900

    とりあえず

--- main.c
+++ main.c
@@ -1,1 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 3
+#define M 2
+#define R 3
+
+
+int i,j,k,u;
+
+void com_AD(int a1,int a2,int b1,int b2){
+    //printf("行列Aの要素 %d %d\n",a1,a2);
+
+    float A[a1][a2];
+    float B[b1][b2];
+
+    if (a2!=b1){
+        printf("この行列は計算できません。\n");
+        exit(0);
+    }
+
+    else{
+        float AB[a1][b2];
+
+        for(i=0;i<N;i++){
+            for(j=0;j<M;j++){
+                for(u=0;u<M;u++){
+                    for(k=0;k<R;k++){
+                        AB[i][u]+=A[i][j]*B[j][k];
+                        }
+                    }
+                
+                }
+            }
+    }
+
+}
