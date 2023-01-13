094d43fcb8d740ea6d02ca288ce0045c313535cc
BATOER-0605 <nem0605@outlook.com>
2023-01-13 11:07:09 +0900

    うんこ

--- main.c
+++ main.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 
+void kuso(FILE *);
+
+int main(){
+    FILE *out = fopen("out.csv","w");
+
+    kuso(out);
+}
+
+void kuso(FILE *fp){
+    printf("へんの長さ: ");
+    float W;
+    scanf("%f",&W);
+
+    printf("\n分割数を入れんかいゴラァ: ");
+    float n;
+    scanf("%f",&n);
+
+    
+}
