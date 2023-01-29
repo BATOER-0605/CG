f79ee571997e9133c7fdee933f25e61b6cdf1655
BATOER-0605 <nem0605@outlook.com>
2023-01-30 02:44:22 +0900

    完成

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    //処理
 
-    int i,j,count=0;
-    int A[5],B[10],Ans[15];
+    int i,j,count=0,flag =0;
+    int A[5],B[10];
 
     FILE *fp = fopen("input.txt","r");
 
     for(i=0;i<10;i++){
 
         fscanf(fp,"%d\n",&B[i]);
-        printf("%d\n",B[i]);
     }
 
     for(i=0;i<5;i++){
@@ -33,57 +31,45 @@ int main(){
     }
     printf("}\n");
 
+    //A U B
     printf("和集合A U B = {");
 
-        //処理
-        for(i=0;i<10;i++){
-            //debug
-            printf("%d\n",B[i]);
-            Ans[i] = B[i];
-        }
+    for(i=0;i<10;i++){
+        printf(" %d",B[i]);
+    }
 
-        //debug
-        for(i=0;i<15;i++){
-            printf("%d ",Ans[i]);
-        }
-/*
-        count = 11;
-
-        for(i=0;i<5;i++){
-            for(j=0;j<10;j++){
-                if(A[i]==B[j]){
-                    continue;
-                }else{
-                    Ans[count] = A[i];
-                    count++;
-                }
-            }
-        }
-
-        if(count < 15){
-            Ans[count] = EOF;
-        }
-        
-
-        for(i=0;i<15;i++){
-            while(Ans[i] != EOF){
-                printf(" %d",Ans[i]);
-            }
-        }
-
-
-    printf("}\n");
-
-    /*
-    printf("積集合 A n B = {");
-    //ｵﾗｵﾗｵﾗｵﾗｵﾗｵﾗｵﾗ!!!!!!
     for(i=0;i<5;i++){
         for(j=0;j<10;j++){
-            if(A[i]==B[j]){printf("%d ",A[i]);}
-            else{continue;}
+            if(A[i] == B[j]){
+                flag =1;
+            }
         }
+        if(flag==0){
+            printf(" %d",A[i]);
+        }
+        flag=0;
     }
+
     printf("}\n");
-    */
+
+    //A n B
+    printf("積集合 A n B = {");
+    
+    flag = 0;
+
+    for(i=0;i<10;i++){
+        for(j=0;j<5;j++){
+            if(A[j] == B[i]){
+                flag =1;
+            }
+        }
+        if(flag==1){
+            printf(" %d",B[i]);
+        }
+        flag=0;
+    }
+
+    printf("}\n");
+    
     fclose(fp);
 }
